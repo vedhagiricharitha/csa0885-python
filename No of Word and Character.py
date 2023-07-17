dedf2ce212671de7e040da8fcdd8ed1c69a3a60b
vedhagiricharitha <139730705+vedhagiricharitha@users.noreply.github.com>
2023-07-17 10:11:25 +0530

    Create No of Word and Character.py

---
+++ No of Word and Character.py
@@ -0,0 +1,11 @@
+str=input("Enter s2:")
+char=0
+word=1
+for i in str:
+      char=char+1
+      if(i==' '):
+            word=word+1
+print("Number of words in the str:")
+print(word)
+print("Number of characters in the str:")
+print(char)
